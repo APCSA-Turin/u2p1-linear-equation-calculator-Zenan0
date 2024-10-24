f977b5adf97c34fcd137c5978554525f53eb5aa5
Zenan0 <zenanl@nycstudents.net>
2024-10-24 04:22:02 +0000

    project 2

---
+++ src/main/java/com/example/project/LinearCalculatorRunner.java
@@ -0,0 +1,33 @@
+package com.example.project;
+
+public class LinearCalculatorRunner {
+    public static void main(String[] args) {
+        // LinearCalculator calc = new LinearCalculator("(1,3)", "(3,4)");
+        // System.out.println(calc.getX1());
+        // System.out.println(calc.getX2());
+        // System.out.println(calc.getY1());
+        // System.out.println(calc.getY2());
+        // System.out.println(calc.distance());
+        // System.out.println(calc.slope());
+        LinearCalculator c2 = new LinearCalculator("(0,0)","(5,68)");
+        System.out.println(c2.printInfo());
+        System.out.println();
+        LinearCalculator c3 = new LinearCalculator("(2,1)","(4,-3)");
+        System.out.println(c3.printInfo());
+        System.out.println();
+        LinearCalculator c33 = new LinearCalculator("(-3,-1)","(4,-3)");
+        System.out.println(c33.printInfo());
+        System.out.println();
+        LinearCalculator c4 = new LinearCalculator("(0,0)","(0,4)");
+        System.out.println(c4.printInfo());
+        System.out.println();
+        LinearCalculator c5= new LinearCalculator("(-5,3)","(2,0)");
+        System.out.println(c5.printInfo());
+        System.out.println();
+        LinearCalculator c6 = new LinearCalculator("(-5,1)","(1,1)");
+        System.out.println(c6.printInfo());
+        System.out.println();
+        LinearCalculator c = new LinearCalculator("(10,5)","(6,-1)");
+        System.out.println(c.printInfo());
+    }
+}
